fea0bfc8a2235d3fb06abe4088d749d9bca6715d
Leoorchid <leoorchid10@gmail.com>
2025-12-18 09:25:43 -0500

    PET done

--- Personal Expense Tracker/main.py
+++ Personal Expense Tracker/main.py
@@ -1,43 +1,82 @@
 import datetime
+import json
+import os
 
 
 print(f"[Personal Expense Tracker]")
-
+fileName = "data.json"
 
 
 class Transaction:
-    
-    def __init__(self,amount,category,description):
-        
+
+    def __init__(self, amount, category, description):
+
         self.amount = amount
         self.category = category
         self.date = datetime.datetime.now().strftime("%B %d, %Y")
         self.description = description
 
-    def view(self):
-        print(f"{self}")
+    def toDict(self):
+        newDict = {
+            "amount": self.amount,
+            "category": self.category,
+            "description": self.description,
+            "date": self.date,
+        }
+        return newDict
 
+def dataToTransaction(data):
+    tran = Transaction(
+        data["amount"],
+        data["category"],
+        data["description"]
+    )
+    tran.date = data["date"]  
+    return tran
+    
 
 class Account:
-    def __init__(self,user,password,income):
-        self.history = []
+    def __init__(self, user, password, income):
         self.user = user
         self.password = password
-        self.spending = 0
         self.income = income
+        self.spending = 0
+        self.history = [] 
 
+        try:
+            with open(fileName, "r") as f:
+                data = json.load(f)
+                for d in data:
+                    self.history.append(dataToTransaction(d))
+        except FileNotFoundError:
+            pass
 
-    def addTransaction(self,amount,category,description):
-        newTrans = Transaction(amount,category,description)
+    def addTransaction(self, amount, category, description):
+        newTrans = Transaction(amount, category, description)
         self.history.append(newTrans)
-    
+        with open(fileName, "w") as f:
+            json.dump([tran.toDict() for tran in self.history], f)
+
     def viewTrans(self):
-        for trans in self.history:
-            print(trans.view)
+        fil = input("Pick filter (1-category,2-date): ")
+
+        match fil:
+            case "1":
+                cat = input("(Need,Want,Other): ")
+                for trans in self.history:
+                    if trans.category == cat:
+                        print(
+                            f"${trans.amount},{trans.category},{trans.description}"
+                        )
+
+            case "2":
+                for i, trans in enumerate(self.history):
+                    print(
+                        f"${trans.amount},{trans.category},{trans.description},{trans.date}"
+                    )
 
 
-user = Account("Leo",123,1000)
-
+user = Account("Leo", 123, 1000)
 
 
 while True:
@@ -48,19 +87,19 @@ while True:
     match action:
         case "1":
             amount = int(input("Amount: $"))
-            category = (input("Need,Want,Other: "))
-            description = (input("Description: "))
-            newTrans = user.addTransaction(amount,category,description)
+            category = input("(1-Need,2-Want,3-Other): ")
+            description = input("Description: ")
+            match category:
+                case "1":
+                    category = "Need"
+                case "2":
+                    category = "Want"
+                case "3":
+                    category = "Other"
+
+            newTrans = user.addTransaction(amount, category, description)
         case "2":
             user.viewTrans()
 
-
-
-        case "Q"|"q":
+        case "Q" | "q":
             pass
-
-
-
-
-
-
